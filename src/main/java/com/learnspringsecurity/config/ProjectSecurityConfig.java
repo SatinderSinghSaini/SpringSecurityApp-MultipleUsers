c6681ed75a7f2021675fadab0321aa55151ec2b4
Satinder Singh Saini <swv095@gmail.com>
2023-04-15 07:39:28 +0530

    with NoOpPasswordEncoder

--- src/main/java/com/learnspringsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/learnspringsecurity/config/ProjectSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -23,17 +25,20 @@ public class ProjectSecurityConfig {
 
     @Bean
     InMemoryUserDetailsManager userDetailsService(){
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
+        UserDetails admin = User.withUsername("admin")
                 .password("12345")
                 .roles("admin")
                 .build();
 
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
+        UserDetails user = User.withUsername("user")
                 .password("12345")
                 .roles("read")
                 .build();
         return new InMemoryUserDetailsManager(admin,user);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
